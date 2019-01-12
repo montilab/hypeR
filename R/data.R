09950cd25d56cba8b7d916455692b4e75a08e5fe
Anthony Federico <anfederico@users.noreply.github.com>
2019-01-12 13:38:09 -0500

    Revamped data handling
    - Automated database updating
    - More granular gene sets
    - Pre-load gmt files into rds objects

---
+++ R/data.R
@@ -0,0 +1,83 @@
+#' @import dplyr msigdbr
+download.gsets <- function(species, category, subcategory) {
+
+    # Download genesets
+    mdf <- msigdbr(species, category, subcategory) %>%
+           dplyr::select(gs_name, gene_symbol) %>%
+           as.data.frame() %>%
+           stats::aggregate(gene_symbol ~ gs_name, data=., c)
+
+    # Convert to list
+    gsets <- as.list(mdf$gene_symbol)
+    names(gsets) <- mdf$gs_name
+    return(gsets)
+}
+
+#' @import dplyr msigdbr
+update.gsets <- function(species="Homo sapiens") {
+
+    cat("Downloading genesets...\n")
+
+    # Gene set categories
+    res <- msigdbr(species=species) %>%
+           dplyr::select(gs_cat, gs_subcat) %>%
+           unique() %>%
+           dplyr::arrange(gs_cat, gs_subcat)
+
+    # Version of data
+    version <- packageVersion("msigdbr")
+
+    for(i in 1:nrow(res)) {
+        category <- as.character(res[i,1])
+        subcategory <- as.character(res[i,2])
+
+        # Download gsets
+        gsets <- download.gsets(species, category, subcategory)
+
+        # Filename formatting
+        nm <- ifelse(subcategory == "", category, paste(category, subcategory, sep="."))
+        nm <- gsub(":", ".", nm)
+        vs <- paste("v", version, sep="")
+        fn <- paste(nm, vs, 'rds', sep=".")
+
+        # Logging
+        cat(paste("-", nm, "->", length(gsets), "Gene Sets", "\n"))
+
+        db <- file.path(system.file("extdata", package="hypeR"), fn)
+        saveRDS(gsets, db)
+    }
+}
+
+#' @export
+db.info <- function() {
+    cat("|------------------------------------------------------------|\n")
+    cat("| Available Gene Sets                                 v6.2.1 |\n")
+    cat("|------------------------------------------------------------|\n")
+    cat("| C1             | Positional (326)                          |\n")
+    cat("| C2.CGP         | Chemical and Genetic Perturbations (3433) |\n")
+    cat("| C2.CP          | Canonical Pathways (252)                  |\n")
+    cat("| C2.CP.BIOCARTA | Canonical BIOCARTA (217)                  |\n")
+    cat("| C2.CP.KEGG     | Canonical KEGG (186)                      |\n")
+    cat("| C2.CP.REACTOME | Canonical REACTOME (674)                  |\n")
+    cat("| C3.MIR         | Motif miRNA Targets (221)                 |\n")
+    cat("| C3.TFT         | Motif Transcription Factor Targets (615)  |\n")
+    cat("| C4.CGN         | Cancer Gene Neighborhoods (427)           |\n")
+    cat("| C4.CM          | Cancer Modules (431)                      |\n")
+    cat("| C5.BP          | GO Biological Process (4436)              |\n")
+    cat("| C5.CC          | GO Cellular Component (580)               |\n")
+    cat("| C5.MF          | GO Molecular Function (901)               |\n")
+    cat("| C6             | Oncogenic Signatures (189)                |\n")
+    cat("| C7             | Immunologic Signatures (4872)             |\n")
+    cat("| H              | Hallmark (50)                             |\n")
+    cat("|------------------------------------------------------------|\n")
+    cat("| Source: http://software.broadinstitute.org/gsea/msigdb     |\n")
+    cat("|------------------------------------------------------------|\n")
+}
+
+#' @export
+db.get <- function(symbol) {
+    path <- paste(symbol, "v6.2.1.rds", sep=".")
+    db <- system.file("extdata", path, package="hypeR")
+    gs <- readRDS(db)
+    return(gs)
+}
---
+++ R/hype.R
@@ -0,0 +1,167 @@
+#' @export
+hypeR <- function(symbols,
+                  gsets,
+                  bg=23467,
+                  min.drawsize=4,
+                  pval.cutoff=1,
+                  fdr.cutoff=1,
+                  verbose=FALSE) {
+
+    cat("Number of genes = ", length(symbols), "\n")
+    cat("Number of gene sets = ", length(gsets), "\n")
+    cat("Background population = ", bg, "\n")
+    cat("P-Value cutoff = ", pval.cutoff, "\n")
+    cat("FDR cutoff = ", fdr.cutoff, "\n")
+
+    df <- data.frame(matrix(ncol=8, nrow=0))
+    colnames(df) <- c("pval","fdr","set.annotated","set.size","category.annotated","total.annotated","category","hits")
+
+    hyp <- hyper.enrichment(drawn=symbols,
+                            categories=gsets,
+                            ntotal=bg,
+                            min.drawsize=min.drawsize,
+                            mht=TRUE,
+                            verbose=verbose)
+
+    # If hits are found format dataframe
+    if (!is.null(hyp)) {
+        df <- data.frame(hyp, stringsAsFactors=F)
+        df[,c(1:6)] <- lapply(df[,c(1:6)], as.numeric)
+        df <- df[complete.cases(df),,drop=F]
+        df <- df[df$pval <= pval.cutoff,,drop=F]
+        df <- df[df$fdr <= fdr.cutoff,,drop=F]
+    }
+    return(df)
+}
+
+#' @import DT
+#' @export
+hyp.show <- function(df, simple=TRUE, stylish=FALSE) {
+    if (simple) {
+        cols <- c(1,2,7,8)
+    } else {
+        cols <- c(1:ncol(df))
+    }
+
+    # Gene symbols converted to hyperlinks
+    url <- "https://www.genecards.org/cgi-bin/carddisp.pl?gene="
+    df$hits <- lapply(df$hits, function(x) {
+                   sapply(symbols, function(x) {
+                       paste('<a href="',
+                             url,
+                             x,
+                             '">',
+                             x,
+                             '</a>',
+                             sep="")
+                   })
+               })
+
+    if (stylish) {
+        datatable(data = df[,cols,drop=F],
+                  style = 'bootstrap',
+                  class = 'table-bordered table-condensed',
+                  escape = TRUE,
+                  rownames = FALSE)
+    } else {
+        datatable(data = df[,cols,drop=F],
+                  escape = TRUE,
+                  rownames = FALSE)
+    }
+}
+
+#' @import openxlsx
+#' @export
+hyp.to.excel <- function(df, file.path, cols=c(1:ncol(df))) {
+    write.xlsx(x = df[,cols,drop=F],
+               file = file.path,
+               col.names = TRUE,
+               row.names = FALSE)
+}
+
+#' @export
+hyp.to.table <- function(df, file.path, sep="\t", cols=c(1:ncol(df))) {
+    write.table(x = df[,cols,drop=F],
+                file = file.path,
+                quote = FALSE,
+                sep = sep,
+                col.names = TRUE,
+                row.names = FALSE)
+}
+
+#' @import plotly
+#' @export
+hyp.plot <- function(df, top=10, val="fdr") {
+
+    # Top pathways
+    df <- head(df, top)
+
+    # Subset data based on significance value
+    if (val == "pval") {
+        df.1 <- df[,c(7,1,5,3)]
+        val.pretty = "P-Value"
+    } else if (val == "fdr") {
+        df.1 <- df[,c(7,2,5,3)]
+        val.pretty = "FDR"
+    }
+
+    # Calculate bar heights
+    colnames(df.1) <- c("y", "x", "x1", "x2")
+    df.2 <- df.1
+    df.2$x <- -log10(df.1$x) # Total bar height
+    df.2$x2 <- df.1$x2/df.1$x1*df.2$x # Second bar height
+    df.2$x1 <- df.2$x-df.2$x2 # First bar height
+    y = factor(df.2$y, levels=df.2$y) # Force order of rownames
+
+    p <- plot_ly(df.2,
+                 x = ~x1,
+                 y = ~y,
+                 type = 'bar',
+                 orientation = 'h',
+                 hoverinfo = 'y',
+                 marker = list(color = '#4CA1AF',
+                               line = list(color = 'white',
+                                           width = 1))) %>%
+
+                 # Split bars
+                 add_trace(x = ~x2, marker = list(color = '#C4E0E5')) %>%
+
+                 # Plot settings
+                 layout(title = "Top Pathways",
+                        xaxis = list(title = paste("-log<sub>10</sub>(",
+                                                   val.pretty,
+                                                   ")",
+                                                   sep=""),
+                                     showgrid = T,
+                                     showline = T,
+                                     showticklabels = T,
+                                     zeroline = T,
+                                     domain = c(0.16, 1)),
+                        yaxis = list(title = "",
+                                     categoryarray = rev(y),
+                                     categoryorder = "array",
+                                     showgrid = T,
+                                     showline = T,
+                                     showticklabels = F,
+                                     zeroline = T),
+                        barmode = 'stack',
+                        paper_bgcolor = 'white',
+                        plot_bgcolor = 'white',
+                        margin = list(l = 300, r = 0, t = 30, b = 40),
+                        showlegend = FALSE) %>%
+
+                 # Labeling the y-axis
+                 add_annotations(xref = 'paper',
+                                 yref = 'y',
+                                 x = 0.15,
+                                 y = y,
+                                 xanchor = 'right',
+                                 text = y,
+                                 categoryorder = "array",
+                                 font = list(family = 'Arial',
+                                             size = 12,
+                                             color = 'black'),
+                                 showarrow = FALSE,
+                                 align = 'right')
+    return(p)
+}
