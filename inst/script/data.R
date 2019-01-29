7a60d0ad979ee7abb155c020803ee5e5f832238d
Anthony Federico <anfederico@users.noreply.github.com>
2019-01-29 14:15:23 -0500

    Script for how data was generated

---
+++ inst/script/data.R
@@ -0,0 +1,7 @@
+library(hypeR)
+
+#### Steps
+# 1. Download the most recent version of msigdb gene sets
+# 2. Separate each gene set by category and subcategory
+# 3. Save each gene set as an rds object in inst/extdata
+update.gsets(species="Homo sapiens")
