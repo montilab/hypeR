53fffe77ead874919d2decd1372271cfd5e84d19
Anthony Federico <anfederico@users.noreply.github.com>
2019-01-28 21:02:52 -0500

    Unit testing

---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+library(testthat)
+library(hypeR)
+
+test_check("hypeR")
+
---
+++ tests/testthat/test-examples.R
@@ -0,0 +1,31 @@
+# Grab all curated genesets
+BIOCARTA <- db.get("C2.CP.BIOCARTA")
+KEGG <- db.get("C2.CP.KEGG")
+REACTOME <- db.get("C2.CP.REACTOME")
+
+# Genes involed in tricarboxylic acid cycle
+symbols <- c("IDH3B","DLST","PCK2","CS","PDHB","PCK1","PDHA1","LOC642502",
+             "PDHA2","LOC283398","FH","SDHD","OGDH","SDHB","IDH3A","SDHC",
+             "IDH2","IDH1","OGDHL","PC","SDHA","SUCLG1","SUCLA2","SUCLG2")
+
+# Hyper enrichment
+hyp <- hypeR(symbols, REACTOME, bg=2520, fdr=0.05)
+
+# Plotting
+p <- hyp.plot(hyp, val="fdr")
+
+test_that("Database retrieval is working", {
+    expect_true(startsWith(names(BIOCARTA)[1], "BIOCARTA"))
+    expect_true(startsWith(names(KEGG)[1], "KEGG"))
+    expect_true(startsWith(names(REACTOME)[1], "REACTOME"))
+})
+
+test_that("Hyper enrichment is working", {
+    expect_equal(hyp$pval, c(3.7e-28, 1.3e-26, 1.0e-18))
+    expect_equal(hyp$fdr, c(2.5e-25, 4.3e-24, 2.2e-16))
+})
+
+test_that("Plotting visualization is working", {
+    expect_s3_class(p, "plotly")
+    expect_s3_class(p, "htmlwidget")
+})
