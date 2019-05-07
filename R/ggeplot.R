7c38ed2e3711756342963999cb47232978d19f5a
Anthony Federico <anfederico@users.noreply.github.com>
2019-05-06 22:52:14 -0400

    Enrichment plots in ggplot

---
+++ R/ggeplot.R
@@ -0,0 +1,25 @@
+#' Enrichment plot implemented in ggplot
+#'
+#' @name ggeplot
+#'
+#' @importFrom ggplot2 qplot aes geom_rug geom_hline geom_vline annotate theme element_text element_blank element_line element_rect
+#' @export
+ggeplot <- function(n, positions, x_axis, y_axis, title="") {
+    score <- which.max(abs(y_axis))
+    qplot(x_axis, 
+          y_axis,
+          main=title,
+          ylab="Running Enrichment Score", 
+          xlab="Position in Ranked List of Genes",
+          geom="line")+
+    geom_rug(data=data.frame(positions), aes(x=positions), inherit.aes=FALSE)+
+    geom_hline(yintercept=0) +
+    geom_vline(xintercept=n/2, linetype="dotted") +
+    annotate("point", x=x_axis[score], y=y_axis[score], color="red") +
+    annotate("text", x=x_axis[score]+n/20, y=y_axis[score], label=round(y_axis[score],2)) +
+    annotate("point", x=x_axis[score], y=y_axis[score], color="red") +
+    theme(plot.title=element_text(hjust=0.5),
+          panel.background=element_blank(), 
+          axis.line=element_line(color="black"),
+          panel.border=element_rect(color="black", fill=NA, size=1))    
+}
---
+++ R/ggvenn.R
@@ -0,0 +1,37 @@
+#' Venn diagram implemented in ggplot
+#'
+#' @name ggvenn
+#'
+#' @importFrom ggplot2 ggplot aes coord_fixed theme_void theme labs element_text
+#' @importFrom ggforce geom_circle
+#' @export
+ggvenn <- function(a, b, ga, gb, title="") {
+   
+    # Calculate area
+    x.a <- length(a)
+    x.b <- length(b)
+    x.u <- length(intersect(a, b))
+    
+    # Normalize area between 0-1
+    nx.a <- x.a/(x.a+x.b)
+    nx.b <- x.b/(x.a+x.b)
+    
+    # Circle radius and diameter
+    r.a <- sqrt(nx.a/pi); d.a <- r.a*2
+    r.b <- sqrt(nx.b/pi); d.b <- r.b*2
+    
+    ol <- ifelse(x.a < x.b, d.a*x.u/x.a, d.b*x.u/x.b)
+    
+    data.frame(x=c(-r.b-r.a+ol, 0),
+               y=c(0, 0),
+               groups=c(paste(ga, " (", x.a, ")", sep=""),
+                        paste(gb, " (", x.b, ")", sep=""))) %>%
+        
+    ggplot(aes(x0=x, y0=y, r=c(r.a, r.b), fill=groups)) +
+    geom_circle(alpha=0.3, size=0.5) +
+    coord_fixed() +
+    theme_void() +
+    theme(plot.title=element_text(hjust=0.5),
+          plot.subtitle=element_text(hjust=0.5)) +
+    labs(title=title, subtitle=paste("Overlap", " (", x.u, ")", sep=""))
+}
