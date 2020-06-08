532695522ece9a0f21f2e10ab9e78c20cbad88f8
Anthony Federico <anfederico@users.noreply.github.com>
2020-06-08 10:22:18 -0400

    Documentation for data

--- R/data.R
+++ R/data.R
@@ -1,9 +1,15 @@
 #' Differential Expression
 #'
 #' A differential expression table
+#' 
+#' @format A data frame
+#' 
 "limma"
 
 #' Co-expression Modules
 #'
 #' A list of co-expression modules
+#' 
+#' @format A nested list of character vectors
+#' 
 "wgcna"
