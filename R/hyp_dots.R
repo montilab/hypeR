8738206a4bc3c10e198b5173169e730309fba468
Teto Monti <smonti@Elisars-iMac.hsd1.ma.comcast.net>
2025-01-28 18:35:02 -0500

    fixed compressed significance legend

--- R/hyp_dots.R
+++ R/hyp_dots.R
@@ -91,7 +91,7 @@
     dplyr::filter(significance <= cutoff) %>%
     ggplot(aes(x = signature, y = label, color = significance, size = size)) +
     geom_point() +
-    scale_color_continuous(low = "#114357", high = "#E53935", trans = .reverselog_trans(10)) +
+    scale_color_continuous(low = "#114357", high = "#E53935", trans = c("log10", "reverse")) +
     labs(title = title, color = color.label) +
     theme(
       plot.title = element_text(hjust = 0.5),
@@ -103,12 +103,8 @@
     p <- p + guides(size = "none")
   } else if (size_by == "significance") {
     p <- p + scale_size_continuous(trans = .reverselog_trans(10)) + labs(size = "Significance")
-  } else if (size_by == "genesets") {
+      } else if (size_by == "genesets") {
     p <- p + scale_size_continuous(trans = scales::log10_trans()) + labs(size = "Genesets\nSize")
-    # p <- p + scale_color_continuous(
-    #   high = "#114357", low = "#E53935", trans = scales::log10_trans(),
-    #   guide = guide_colorbar(reverse = TRUE)
-    #)
   }
   return(p)
 }
