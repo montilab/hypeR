e995fe25ef49904ba11c3fe3143fd8938377bbce
Mengze Li (Vanessa) <vmli@bu.edu>
2025-05-08 18:27:20 -0400

    update msigdbr functions
    previous "category" and "subcategory" is replaced by collection and subcollection

--- R/db_msig.R
+++ R/db_msig.R
@@ -63,7 +63,7 @@ msigdb_available <- function(species="Homo sapiens") {
     dplyr::select(gs_cat, gs_subcat) %>%
     unique() %>%
     dplyr::arrange(gs_cat, gs_subcat) %>%
-    magrittr::set_colnames(c("Category", "Subcategory"))
+    magrittr::set_colnames(c("collection", "subcollection"))
 }
 
 #' Print msigdb gsets information
@@ -96,25 +96,25 @@ msigdb_info <- function() {
     cat("|------------------------------------------------------------------|\n")
     cat("| Available Genesets                                               |\n")
     cat("|------------------------------------------------------------------|\n")
-    cat("| Category Subcategory | Description                               |\n")    
+    cat("| Collection Subcollection | Description                           |\n")    
     cat("|------------------------------------------------------------------|\n")
-    cat("| C1                   | Positional                                |\n")
-    cat("| C2 CGP               | Chemical and Genetic Perturbations        |\n")
-    cat("| C2 CP                | Canonical Pathways                        |\n")
-    cat("| C2 CP:BIOCARTA       | Canonical BIOCARTA                        |\n")
-    cat("| C2 CP:KEGG           | Canonical KEGG                            |\n")
-    cat("| C2 CP:PID            | Canonical PID                             |\n")
-    cat("| C2 CP:REACTOME       | Canonical REACTOME                        |\n")
-    cat("| C3 MIR               | Motif miRNA Targets                       |\n")
-    cat("| C3 TFT               | Motif Transcription Factor Targets        |\n")
-    cat("| C4 CGN               | Cancer Gene Neighborhoods                 |\n")
-    cat("| C4 CM                | Cancer Modules                            |\n")
-    cat("| C5 BP                | GO Biological Process                     |\n")
-    cat("| C5 CC                | GO Cellular Component                     |\n")
-    cat("| C5 MF                | GO Molecular Function                     |\n")
-    cat("| C6                   | Oncogenic Signatures                      |\n")
-    cat("| C7                   | Immunologic Signatures                    |\n")
-    cat("| H                    | Hallmark                                  |\n")
+    cat("| C1                       | Positional                            |\n")
+    cat("| C2 CGP                   | Chemical and Genetic Perturbations    |\n")
+    cat("| C2 CP                    | Canonical Pathways                    |\n")
+    cat("| C2 CP:BIOCARTA           | Canonical BIOCARTA                    |\n")
+    cat("| C2 CP:KEGG               | Canonical KEGG                        |\n")
+    cat("| C2 CP:PID                | Canonical PID                         |\n")
+    cat("| C2 CP:REACTOME           | Canonical REACTOME                    |\n")
+    cat("| C3 MIR                   | Motif miRNA Targets                   |\n")
+    cat("| C3 TFT                   | Motif Transcription Factor Targets    |\n")
+    cat("| C4 CGN                   | Cancer Gene Neighborhoods             |\n")
+    cat("| C4 CM                    | Cancer Modules                        |\n")
+    cat("| C5 BP                    | GO Biological Process                 |\n")
+    cat("| C5 CC                    | GO Cellular Component                 |\n")
+    cat("| C5 MF                    | GO Molecular Function                 |\n")
+    cat("| C6                       | Oncogenic Signatures                  |\n")
+    cat("| C7                       | Immunologic Signatures                |\n")
+    cat("| H                        | Hallmark                              |\n")
     cat("|------------------------------------------------------------------|\n")
     cat("| Source: http://software.broadinstitute.org/gsea/msigdb           |\n")
     cat("|------------------------------------------------------------------|\n")
@@ -123,8 +123,8 @@ msigdb_info <- function() {
 #' Download data from msigdb in the form of a named list
 #'
 #' @param species A species to determine gene symbols (refer to ?msigdbr::msigdbr for avilable species)
-#' @param category Geneset category (refer to ?msigdbr::msigdbr for avilable categories)
-#' @param subcategory Geneset subcategory (refer to ?msigdbr::msigdbr for avilable subcategories)
+#' @param collection Geneset collection (previous argument "category" is deprecated. refer to ?msigdbr::msigdbr for avilable collections)
+#' @param subcollection Geneset subcollection (previous argument "subcategory" is deprecated. refer to ?msigdbr::msigdbr for avilable subcollections)
 #' @return A list of genesets
 #'
 #' @examples
@@ -134,18 +134,18 @@ msigdb_info <- function() {
 #' @importFrom dplyr select
 #' @importFrom msigdbr msigdbr
 #' @export
-msigdb_download <- function(species, category, subcategory="") {
+msigdb_download <- function(species, collection, subcollection="") {
     
     # Check species
     msigdb_check_species(species)
     
-    response <- msigdbr(species, category, subcategory)
+    response <- msigdbr(species, collection, subcollection)
     if (nrow(response) == 0) {
         stop("No data found: Please review available species and genesets\n", msigdb_info())
     }
     
     # Download genesets
-    mdf <- msigdbr(species, category, subcategory) %>%
+    mdf <- msigdbr(species, collection, subcollection) %>%
         dplyr::select(gs_name, gene_symbol) %>%
         dplyr::distinct()
     
@@ -157,8 +157,8 @@ msigdb_download <- function(species, category, subcategory="") {
 #' Download data from msigdb in the form of a gsets object
 #'
 #' @param species A species to determine gene symbols (refer to ?msigdbr::msigdbr for avilable species)
-#' @param category Geneset category (refer to ?msigdbr::msigdbr for avilable categories)
-#' @param subcategory Geneset subcategory (refer to ?msigdbr::msigdbr for avilable subcategories)
+#' @param collection Geneset collection (previous argument "category" is deprecated. refer to ?msigdbr::msigdbr for avilable collections)
+#' @param subcollection Geneset subcollection (previous argument "subcategory" is deprecated. refer to ?msigdbr::msigdbr for avilable subcollections)
 #' @param clean Use true to clean labels of genesets
 #' @return A gsets object
 #'
@@ -166,9 +166,9 @@ msigdb_download <- function(species, category, subcategory="") {
 #' HALLMARK <- msigdb_gsets("Homo sapiens", "H", "")
 #'
 #' @export
-msigdb_gsets <- function(species, category, subcategory="", clean=FALSE) {
-    genesets <- msigdb_download(species, category, subcategory)
-    name <- ifelse(subcategory == "", category, paste(category, subcategory, sep="."))
+msigdb_gsets <- function(species, collection, subcollection="", clean=FALSE) {
+    genesets <- msigdb_download(species, collection, subcollection)
+    name <- ifelse(subcollection == "", collection, paste(collection, subcollection, sep="."))
     version <- msigdb_version()
     gsets$new(genesets, name=name, version=version, clean=clean)
 }
